a6807b4ed7cf6d1945de2ecc4caee0afe04c48a4
gph <gpenghe@gmail.com>
2013-09-01 16:36:36 -0400

    updated .screerc and pimirror.sh

--- bin/pimirror.sh
+++ bin/pimirror.sh
@@ -1,6 +1,6 @@
 rsync  \
     --progress \
+    --stats \
     --archive --verbose --delete --delete-delay --delay-updates \
     archive.raspbian.org::archive raspbian
 #    -normal \
-#    --stats \
