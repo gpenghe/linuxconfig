ed38ce4fbc9f91908775b3a5e7b6fdc6053ea0b8
gph <gpenghe@gmail.com>
2015-03-31 11:17:54 -0400

    share.sh added

---
+++ bin/share.sh
@@ -0,0 +1,24 @@
+if [ -z "$PROJ" ]; then
+    echo "Must define PROJ with one of the following:"
+    echo "      TrustedComputing"
+    echo "      enics"
+    exit -1
+fi
+
+if [ $# == 0 ]; then
+    echo "Usage: share.sh <files>"
+    exit -1
+fi
+
+echo Sharing "$*" to $PROJ
+git_dir=$HOME/shared-data/$PROJ
+if ! [ -d "$git_dir" ]; then
+    echo "$git_dir doesn't exist."
+    exit -1
+fi
+
+cp -a "$*" $git_dir
+cd $git_dir
+git add --all .
+git commit -m "New shared data: $*"
+
