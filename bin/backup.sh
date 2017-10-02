777f5b6040774c2f9ca4ce409225d2982ae6c0a3
gph <gpenghe@gmail.com>
2017-10-02 14:31:46 -0400

    fix backup.sh for backup directory

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 
 date=$(date +"%Y-%m-%d_%H-%M-%S")
-backupdir="$HOME/Backup"
+test -e $HOME/Backup || test -e $HOME/backup || mkdir $HOME/Backup
+test -e $HOME/Backup && backupdir=$HOME/Backup || backupdir=$HOME/backup
 test "$1" || { echo "Must provide an argument" ; exit 1; }
 source=$1
 name=$(basename $source)  # remove the possible trailing "/"
