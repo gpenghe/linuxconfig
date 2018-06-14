10f95088a6c155d9be08a44f628e4ad620f51977
xiaobai <gpenghe@gmail.com>
2018-06-13 23:12:53 -0400

    add losetup

--- snippets/LinuxCommand_loop_mount.sh
+++ snippets/LinuxCommand_loop_mount.sh
@@ -1,5 +1,17 @@
-  # sudo losetup -f -P -r --show LENS_runing_from_SD.img
-  # losetup -l
-  # sudo mount  /dev/loop0p2 p2
-  # sudo mount  /dev/loop0p1 p1
-  # sudo losetup -D
+# method 1
+sudo losetup -f -P -r --show imagename.img
+losetup -l
+sudo mount  /dev/loop0p2 p2
+sudo mount  /dev/loop0p1 p1
+sudo losetup -D
+
+# method 2
+sudo fdisk -l imagename.img
+# take note of the START sector for the second partition
+# Example:
+#         Device Boot      Start         End      Blocks   Id  System
+# imagename.img1            8192      122879       57344    c  W95 FAT32 (LBA)
+# imagename.img2          122880    15415295     7646208   83  Linux
+# 
+# Then the start is 122880
+sudo losetup /dev/loop0 imagename.img -o $((START*512))
