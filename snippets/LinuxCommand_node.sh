70061e71fd3a5afdc37ba847a24438655e8b2e1d
xiaobai <gpenghe@gmail.com>
2018-06-23 23:41:35 -0400

    snippets: add node

---
+++ snippets/LinuxCommand_node.sh
@@ -0,0 +1,15 @@
+# upgrade npm
+
+npm install npm@latest
+
+# install globally
+
+npm install {{tldr}} -g
+
+# ignore permission errors
+
+npm install {{tldr}} --unsafe-perm
+
+# Upgrade node
+npm install n -g
+n 10.5.0
