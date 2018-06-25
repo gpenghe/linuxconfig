fb0aac421d58a65048e17dd4d1362cd1d6a295f5
xiaobai <gpenghe@gmail.com>
2018-06-25 09:35:50 -0400

    snippets: add LinuxCommand_npm.sh

---
+++ snippets/LinuxCommand_npm.sh
@@ -0,0 +1,16 @@
+# upgrade npm
+
+npm install npm@latest
+(npm@6.1.0)
+
+# install globally
+
+npm install {{tldr}} -g
+
+# ignore permission errors
+
+npm install {{tldr}} --unsafe-perm
+
+# Upgrade node
+npm install n -g
+n 10.5.0
