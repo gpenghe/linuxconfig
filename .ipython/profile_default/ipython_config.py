7f16b62a297eaf1cbcd9f088c3aacc8edec1e12d
Penghe Geng <penghe.geng@smith-nephew.com>
2018-11-29 10:06:41 -0500

    ignore ipython powerline prompt error

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,7 @@
-from powerline.bindings.ipython.since_5 import PowerlinePrompts
-c = get_config()
-c.TerminalInteractiveShell.simple_prompt = False
-c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+try:
+    from powerline.bindings.ipython.since_5 import PowerlinePrompts
+    c = get_config()
+    c.TerminalInteractiveShell.simple_prompt = False
+    c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+except:
+    pass
