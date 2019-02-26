726b14e60b3885f23840445395defb372a12a720
Penghe Geng <penghe.geng@smith-nephew.com>
2019-02-26 16:51:58 -0500

    update saferm.sh

--- bin/saferm.sh
+++ bin/saferm.sh
@@ -1,39 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-#echo $0 $*
-
-has_important_file=0;
+has_important_file=0
 use_rf=0
-protect=0
-hgfolder=0;
 
 for item in $*; do
 	if [ $item = '-rf' ]; then
 		use_rf=1;
-	fi
-	if [ ${item%%hg} != $item ] || [ ${item%%ckup} != $item ] || [ -d ${item}/.hg ]; then
+    fi
+    if [[ $item == "build" ]] || [[ $item == "build/" ]] || [ -d ${item}/.git ]; then
 		has_important_file=1;
-		if [ -z $important_file ]; then
-			important_file="$item"
-		else
-			important_file="$item $important_file"
-		fi
-		if [ -d $item/.hg ]; then
-			hgfolder=1;
-		fi
+        important_files="$item $important_files"
 	fi
 done
 
-if [ $has_important_file -eq 1 ] && [ $use_rf -eq 1 ]; then
-	protect=1;
-fi
-
-if [ $protect -eq 1 ]; then # Doing the confirmation
-	[ ${hgfolder:-0} -eq 1 ] && echo "$important_file contain(s) .hg!"
-	echo "Do you really want to delete: $important_file?"
+if [ $has_important_file -eq 1 ] && [ $use_rf -eq 1 ]; then  # Doing the confirmation
+	echo "Do you really want to delete: $important_files?"
 	echo -n "Type YES to confirm: "
-	read input;
-	if [ -n $input ] && [ $input = "YES" ]; then
+	read input
+	if [[ -n $input ]] && [[ $input == "YES" ]]; then
 		echo rm -i $*
 		rm -i $*
 	else
@@ -44,5 +28,3 @@ else # No need to protect
 	echo rm -i $*
 	rm -i $*
 fi
-
-		
